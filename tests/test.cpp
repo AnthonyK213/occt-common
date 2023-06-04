606f97296c75796fbd059c2fd37198f26de26b73
AnthonyK213 <tjjzqwh@163.com>
2023-06-05 03:14:43 +0800

    separate

--- tests/test.cpp
+++ tests/test.cpp
@@ -1,27 +1,4 @@
-#include <OcctCommon/Geometry/Point3d.h>
-#include <gtest/gtest.h>
-
-using namespace OcctCommon::Geometry;
-
-TEST(GeometryTests, point3d_eq) {
-  Point3d pt1(1, 2, 3);
-  Point3d pt2(1, 2, 3);
-  ASSERT_TRUE(pt1 == pt2);
-}
-
-TEST(GeometryTests, point3d_mut) {
-  Point3d pt(1, 2, 3);
-  gp_Pnt &pnt = pt.PntMut();
-  pnt.SetX(10);
-  std::cout << pt.X() << std::endl;
-  ASSERT_EQ(pt.X(), 10);
-}
-
-TEST(GeometryTests, point3d_add) {
-  Point3d pt1(1, 2, 3);
-  Point3d pt2(-1, -2, -3);
-  ASSERT_TRUE(Point3d::Add(pt1, pt2) == Point3d::Origin());
-}
+#include "Point3d_Test.h"
 
 int main(int argc, char *argv[]) {
   ::testing::InitGoogleTest(&argc, argv);
